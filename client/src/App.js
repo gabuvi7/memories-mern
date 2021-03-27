57ed1d1632742fe70b9adef9be4c24ff55cac28b
Gabriel <uviedo_gabriel@hotmail.com>
2021-03-27 20:29:30 -0300

    added reducers, and useSelector into the posts component

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,16 @@ import memories from "./images/memories.jpg";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getPosts } from "./actions/posts";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
