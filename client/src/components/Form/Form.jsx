64498c4cfa16b6d709d60f7a35fdf0c0fbb9407d
Gabriel <uviedo_gabriel@hotmail.com>
2021-03-28 11:46:10 -0300

    added form component

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import { useState } from "react";
+import FileBase from 'react-file-base64'
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -16,23 +17,51 @@ const Form = () => {
     setPostData(e.target.value);
   };
 
-  const handleChange = (e, type) => {
-    setPostData({ ...postData, type: e.target.value }); // it's provide all content of data, so if we have a lot of textfield, it can be fully with it and not replace with the new data.
+  const handleChange = (type, base64) => (e) => {
+    switch (type) {
+      case "Creator":
+        setPostData({ ...postData, creator: e.target.value }); // it's provide all content of data, so if we have a lot of textfield, it can be fully with it and not replace with the new data.
+        break;
+      case "Title":
+        setPostData({ ...postData, title: e.target.value });
+        break;
+      case "Message":
+        setPostData({ ...postData, message: e.target.value });
+        break;
+      case "Tags":
+        setPostData({ ...postData, tags: e.target.value });
+        break;
+      case "File":
+        console.log(base64)
+        setPostData({ ...postData, selectedFile: base64 });
+        break;
+      default: 
+        break;
+    }
   };
+
+  const clearForm = () => {
+    
+  }
 
   return (
     <Paper className={classes.paper}>
       <form
-        autocomplete="off"
+        autoComplete="off"
         noValidate
         className={classes.form}
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">Creating a Memory</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.Creator} onChange={handleChange}></TextField>
-        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={handleChange}></TextField>
-        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={handleChange}></TextField>
-        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={handleChange}></TextField>
+        <TextField style={{marginBottom: "1rem"}} name="creator" variant="outlined" label="Creator" fullWidth value={postData.Creator} onChange={handleChange('Creator')} ></TextField>
+        <TextField style={{marginBottom: "1rem"}} name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={handleChange('Title')} ></TextField>
+        <TextField style={{marginBottom: "1rem"}} name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={handleChange('Message')} ></TextField>
+        <TextField style={{marginBottom: "1rem"}} name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={handleChange('Tags')} ></TextField>
+        <div className={classes.fileInput}>
+          <FileBase type="file" multiple={false} onDone={(base64) => handleChange(base64, 'File')} ></FileBase>
+        </div>
+        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth >Submit</Button>
+        <Button variant="contained" color="secondary" size="small" onClick={clearForm} fullWidth >Clear form</Button>
       </form>
     </Paper>
   );
