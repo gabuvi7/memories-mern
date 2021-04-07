e06078d9e292a72d4dcf6f00561f32dfd387e38d
Gabriel <uviedo_gabriel@hotmail.com>
2021-04-06 22:31:07 -0300

    added auth, home and navbar component, added routes, browser and switch into the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,20 @@
-import { Container, Grow, Grid } from "@material-ui/core";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { getPosts } from "./actions/posts";
-import { useResizeScreen } from "./customHooks/useResizeScreen";
+import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
 
 function App() {
-  const dispatch = useDispatch();
-  const [currentId, setCurrentId] = useState(null);
-  const { screen } = useResizeScreen();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-            style={{
-              flexDirection: `${screen.sWidth <= 600 ? "column-reverse" : ""}`,
-            }}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
